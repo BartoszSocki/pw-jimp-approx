63729118a82e140a2049a5cfa4940ed639dc62c0
Inga <ingamaziarz@wp.pl>
2022-01-19 22:12:50 +0100

    makefile + makesplcheb

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+chebyshev: main.o splines.o points.o chebyshev_approx.o gaus/libge.a
+	$(CC) -o chebyshev main.o splines.o points.o chebyshev_approx.o -L gaus -l ge 
+
 aprox: main.o splines.o points.o aproksymator_na_bazie.o gaus/libge.a
 	$(CC) -o aprox  main.o splines.o points.o aproksymator_na_bazie.o -L gaus -l ge
 
@@ -10,10 +13,13 @@ prosta: main.o splines.o points.o prosta.o
 aproksymator_na_bazie.o: makespl.h points.h gaus/piv_ge_solver.h
 	$(CC) -I gaus -c aproksymator_na_bazie.c
 
+chebyshev_approx.o: makespl.h points.h gaus/piv_ge_solver.h
+	$(CC) -I gaus -c chebyshev_approx.c
+
 interpolator.o: makespl.h points.h gaus/piv_ge_solver.h
 	$(CC) -I gaus -c interpolator.c
 
 .PHONY: clean
 
 clean:
-	-rm *.o aprox intrp prosta
+	-rm *.o aprox intrp prosta chebyshev
--- chebyshev_approx.c
+++ chebyshev_approx.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
+
 
 double T (int n, double x){ //chebyshev polynomial of first kind
 	if (n==0)
@@ -28,16 +30,78 @@ double dT(int n, double x){
 
 double dU(int n, double x){
 	return ((n+1)*T(n+1, x) - x*U(n, x))/(x*x - 1);
-
+}
 double d2T(int n, double x) {
 	return n*((n*T(n,x)-x*U(n-1, x))/(x*x - 1));
 }
 
-double d3T(double a, double b, int n, int i, double x) {
+double d3T(int n,  double x) {
 	return n*(((n*dT(n,x)-x*dU(n-1, x)-U(n-1, x))*(x*x-1) - (n*T(n, x) - x*U(n-1, x))*2*x)/(x*x*x*x - 2*x*x + 1)); 
+}
+double xfi(double a, double b, int n, int i, FILE * out)
+{
+	double h = (b - a) / (n - 1);
+	double h3 = h * h * h;
+	int hi[5] = { i - 2, i - 1, i, i + 1, i + 2 };
+	double hx[5];
+	int j;
+
+	for (j = 0; j < 5; j++)
+		hx[j] = a + h * hi[j];
+
+	fprintf(out, "# nb=%d, i=%d: hi=[", n, i);
+	for (j = 0; j < 5; j++)
+		fprintf(out, " %d", hi[j]);
+	fprintf(out, "] hx=[");
+	for (j = 0; j < 5; j++)
+		fprintf(out, " %g", hx[j]);
+	fprintf(out, "]\n");
 }
 
 void make_spl(points_t * pts, spline_t * spl) {
+  matrix_t *eqs = NULL;
+  double *x = pts->x;
+  double *y = pts->y;
+  double a = x[0];
+  double b = x[pts->n - 1];
+  int i, j, k;
+  int nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
+  char *nbEnv = getenv("APPROX_BASE_SIZE");
 
+  if (nbEnv != NULL && atoi(nbEnv) > 0)
+    nb = atoi(nbEnv);
+
+  eqs = make_matrix(nb, nb + 1);
+  for (j = 0; j < nb; j++) {
+    for (i = 0; i < nb; i++)
+      for (k = 0; k < pts->n; k++)
+        add_to_entry_matrix(eqs, j, i, T(i, x[k]) * T(j, x[k]));
+
+    for (k = 0; k < pts->n; k++)
+      add_to_entry_matrix(eqs, j, nb, y[k] * T(j, x[k]));
+  }
+
+  if (piv_ge_solver(eqs)) {
+    spl->n = 0;
+    return;
+  }
+
+  if (alloc_spl(spl, nb) == 0) {
+    for (i = 0; i < spl->n; i++) {
+      double xx = spl->x[i] = a + i * (b - a) / (spl->n - 1);
+      xx += 10.0 * DBL_EPSILON;  //by nie ulokować punktu w poprz. przedziale
+      spl->f[i] = 0;
+      spl->f1[i] = 0;
+      spl->f2[i] = 0;
+      spl->f3[i] = 0;
+      for (k = 0; k < nb; k++) {
+        double ck = get_entry_matrix(eqs, k, nb);
+        spl->f[i] += ck * T (k, xx);
+        spl->f1[i] += ck * dT (k, xx);
+        spl->f2[i] += ck * d2T(k, xx);
+        spl->f3[i] += ck * d3T(k, xx);
+      }
+    }
+  }
 }
 
